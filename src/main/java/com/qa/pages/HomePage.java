cc2e16db22438d1a54d3f2a4f5a17bec17f7642c
unknown <udayasai@gmail.com>
2018-07-10 11:25:56 +0530

    search page added and modifed home page

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,4 +5,8 @@ public class HomePage {
 	public void homepage() {
 		System.out.println("home page");
 	}
+	
+	public void test2() {
+		System.out.println("test2");
+	}
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class SearchPage {
+	public void searchpage() {
+		System.out.println("search page");
+	}
+}
